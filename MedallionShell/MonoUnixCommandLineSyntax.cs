9806c76c61a1595aa7f25624619983b8d8f7a250
Michael Adelson <mike.adelson314@gmail.com>
2019-04-17 07:10:14 -0400

    Take a different tack on MonoUnix syntax

--- MedallionShell/MonoUnixCommandLineSyntax.cs
+++ MedallionShell/MonoUnixCommandLineSyntax.cs
@@ -12,7 +12,24 @@ namespace Medallion.Shell
         {
             var windowsResult = WindowsSyntax.CreateArgumentString(arguments);
             // recommended https://bugzilla.xamarin.com/show_bug.cgi?id=19296
-            return windowsResult.Replace("\\$", "\\\\$").Replace("\\`", "\\\\`");
+            //return windowsResult.Replace("\\$", "\\\\$").Replace("\\`", "\\\\`");
+
+            // from https://github.com/GNOME/glib/blob/master/glib/gshell.c
+
+            var stringBuilder = new StringBuilder();
+            foreach (var argument in arguments)
+            {
+                if (stringBuilder.Length > 0) { stringBuilder.Append(' '); }
+                stringBuilder.Append('\'');
+                foreach (var @char in argument)
+                {
+                    if (@char == '\'') { stringBuilder.Append(@"'\'"); }
+                    else { stringBuilder.Append(@char); }
+                }
+                stringBuilder.Append('\'');
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
