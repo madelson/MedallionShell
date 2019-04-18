c3ed51c6fd819f9ae65a77dd288e2ecbfddb3215
Michael Adelson <mike.adelson314@gmail.com>
2019-04-18 19:36:44 -0400

    Revert log changes

--- MedallionShell/Log.cs
+++ MedallionShell/Log.cs
@@ -10,18 +10,16 @@ namespace Medallion.Shell
 {
     internal static class Log
     {
-        // TODO REVERT CHANGES
         [Conditional("TESTING")]
         public static void WriteLine(string format, params object[] args)
         {
-            Console.WriteLine(format, args);
-            //var text = string.Format("{0:h:m:ss.fff}: ", DateTime.Now) + string.Format(format, args);
+            var text = string.Format("{0:h:m:ss.fff}: ", DateTime.Now) + string.Format(format, args);
 
-            //using (var fs = new FileStream("log.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
-            //using (var writer = new StreamWriter(fs))
-            //{
-            //    writer.WriteLine(text);
-            //}
+            using (var fs = new FileStream("log.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+            using (var writer = new StreamWriter(fs))
+            {
+                writer.WriteLine(text);
+            }
         }
     }
 }
