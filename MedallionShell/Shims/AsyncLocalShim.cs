0d40d6e1fd6ef59476b382a7fade8223b66f590f
Michael Adelson <mike.adelson314@gmail.com>
2023-03-07 07:37:45 -0500

    Build fix

--- MedallionShell/Shims/AsyncLocalShim.cs
+++ MedallionShell/Shims/AsyncLocalShim.cs
@@ -1,5 +1,6 @@
 ﻿// note: we use the shim implementation for our NETFRAMEWORK tests in DEBUG just to get coverage
 #if NET45 || (DEBUG && NETFRAMEWORK)
+#pragma warning disable SA1649 // File name should match first type name
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,11 @@ internal sealed class AsyncLocal<T> where T : class? // needed for ConditionalWe
     // CallContext values must be serializable; this indirection should eliminate any chance of that burning us
     private static readonly ConditionalWeakTable<object, T> Storage = new();
 
-    private readonly string Key = $"AsyncLocal<{typeof(T)}>_{Guid.NewGuid()}";
+    private readonly string key = $"AsyncLocal<{typeof(T)}>_{Guid.NewGuid()}";
 
     public T? Value
     {
-        get => CallContext.LogicalGetData(Key) is { } storageKey 
+        get => CallContext.LogicalGetData(this.key) is { } storageKey 
             ? Storage.TryGetValue(storageKey, out var value) 
                 ? value 
                 : throw new KeyNotFoundException() 
@@ -29,7 +30,7 @@ internal sealed class AsyncLocal<T> where T : class? // needed for ConditionalWe
             object? storageKey;
             if (value is null) { storageKey = null; }
             else { Storage.Add(storageKey = new(), value); }
-            CallContext.LogicalSetData(Key, storageKey);
+            CallContext.LogicalSetData(this.key, storageKey);
         }
     }
 }
