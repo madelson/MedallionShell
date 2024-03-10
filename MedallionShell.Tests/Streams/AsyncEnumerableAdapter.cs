f6a555a46ae432121028311cf0b3e0c8f81a101c
Jihoon Park <bartleby2718@gmail.com>
2024-03-09 22:59:37 -0500

    Revert primary constructor in AsyncEnumerableAdapter

--- MedallionShell.Tests/Streams/AsyncEnumerableAdapter.cs
+++ MedallionShell.Tests/Streams/AsyncEnumerableAdapter.cs
@@ -4,10 +4,17 @@ using System.Threading.Tasks;
 
 namespace Medallion.Shell.Tests.Streams;
 
-public class AsyncEnumerableAdapter(IEnumerable<string> enumerable) : IAsyncEnumerable<string>
+public class AsyncEnumerableAdapter : IAsyncEnumerable<string>
 {
+    private readonly IEnumerable<string> strings;
+
+    public AsyncEnumerableAdapter(IEnumerable<string> strings)
+    {
+        this.strings = strings;
+    }
+
     public IAsyncEnumerator<string> GetAsyncEnumerator(CancellationToken cancellationToken = default) =>
-        new AsyncEnumeratorAdapter(enumerable.GetEnumerator());
+        new AsyncEnumeratorAdapter(this.strings.GetEnumerator());
 
     private class AsyncEnumeratorAdapter(IEnumerator<string> enumerator) : IAsyncEnumerator<string>
     {
